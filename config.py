c36504580cc927a27408e979be3af6260b4a952d
Stultus <hrishi.kb@gmail.com>
2014-12-07 14:09:14 +0530

    added basic config file

---
+++ config.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+CSRF_ENABLED = True
+SECRET_KEY='parayan-manasilla'
+
+
